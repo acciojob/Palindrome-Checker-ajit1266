d432efde950ba3b86b8c72a900c588225ac144a1
ajit1266 <100941072+ajit1266@users.noreply.github.com>
2022-12-04 11:36:54 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,7 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-    const ltrArray = string.split('');
+    const ltrArray = str.split('');
     const rtlArray = ltrArray.reverse();
     const reverseStr = rtlArray.join('');
     if(str != reverseStr) {
