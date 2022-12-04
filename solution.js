1664fbac05df18b4054d2860e051bede5a49f1a2
ajit1266 <100941072+ajit1266@users.noreply.github.com>
2022-12-04 11:17:52 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,12 +1,5 @@
 // complete the given function
 
 function palindrome(str){
-const ltorArray = str.split('');
-const rtolArray = ltorArray.reverse();
-const rtolString = ltorArray.join('');
-if(str == rtolString) {
-    return true;
-  }
- else return false;
 }
 module.exports = palindrome
