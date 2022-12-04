90c825e3000907766ae73698ed516d4f8d4a3a86
ajit1266 <100941072+ajit1266@users.noreply.github.com>
2022-12-04 11:22:25 +0530

    palindromeLogic

--- solution.js
+++ solution.js
@@ -1,5 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+    const ltrArray = string.split('');
+    const rtlArray = ltrArray.reverse();
+    const reverseStr = rtlArray.join('');
+    if(str !== reverseStr) {
+        return false
+    }
+  return true;
 }
 module.exports = palindrome
